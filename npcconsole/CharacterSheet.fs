be275f68e22a83dc7016c70f5c0d8fa79b4e6159
Alex Holloway <alex.holloway.ctx@gmail.com>
2019-10-28 22:14:46 +0000

    Abstract character sheet for formatting

---
+++ npcconsole/CharacterSheet.fs
@@ -0,0 +1,144 @@
+namespace NpcConsole
+
+open System
+open NpcConsole.Attributes
+
+// A character sheet is a baking-down of a character into a printable list of
+// items under sub-headings.
+// Should be good for various display UIs (?)
+type CharacterSubHeading = {
+    Title: string option
+    Items: (string * string) list
+}
+
+module CharacterSheet =
+    let formatLevel c = Some (sprintf "Level %d" c.Level)
+    let formatClass c = match c.Class with | Some cl -> Some (sprintf "%A" cl) | None -> None
+
+    let one (n, v) = n, v.ToString ()
+    let many (n, vs) = n, String.Join (" ", vs |> List.map (fun v -> v.ToString ()))
+
+    let printTitle c =
+        let elements =
+            [formatLevel c; c.Heritage; c.Ancestry; formatClass c]
+            |> List.choose id
+        many ("Is a", elements)
+
+    let printSkill c sk =
+        let rank = (Derive.rank sk c).ToString ()
+        let bonus = Derive.bonus sk c
+        one (sk.Name, sprintf "%+3d %15s (%15s)" bonus rank (sk.KeyAbility.ToString ()))
+
+    let printDC c sk =
+        let rank = (Derive.rank sk c).ToString ()
+        let dc = Derive.bonus sk c |> Derive.dc
+        one ((sprintf "%s DC" sk.Name), sprintf "%3d %15s (%15s)" dc rank (sk.KeyAbility.ToString ()))
+
+    let printArmorClass c = [
+        match c.Armor with
+        | Some a ->
+            let rank = (Derive.rank a.Skill c).ToString ()
+            let ac = Derive.armorClass c
+            yield one ("Armor Class", sprintf "%3d %15s" ac rank)
+            yield one ("Armor Type", a.Name)
+            yield many ("Armor Traits", a.Traits |> List.map (fun t -> sprintf "%A" t))
+        | None -> ()
+    ]
+    
+    let printWeaponStats (weapon, c) = [
+        match weapon with
+        | Some w ->
+            // TODO Dexterity bonus on melee weapon attack, sometimes
+            let sk = Skills.weaponSkill w
+            yield printSkill c sk
+            // TODO When do ranged weapons gain a damage bonus?  From what?  I forget...
+            let damageModifier =
+                match w.Type with
+                | Melee -> Map.find Strength c.Abilities |> Derive.modifier
+                | Ranged -> 0<Modifier>
+            yield one (sprintf "%s damage" w.Name, sprintf "%A %+2d (%A)" w.Damage damageModifier w.DamageType)
+            match w.Range with
+            | Some r -> yield one (sprintf "%s Range" w.Name, sprintf "%3d" r)
+            | None -> ()
+            if w.Reload > 0<Actions> then yield one (sprintf "%s Reload" w.Name, sprintf "%3d" w.Reload) else ()
+            yield many (sprintf "%s traits" w.Name, w.Traits |> List.map (fun t -> sprintf "%A" t))
+        | None -> ()
+    ]
+
+    let printClassDC c = [
+        match Map.tryPick (fun (sk: Skill) _ -> if sk.Name.Contains ("Class") then Some sk else None) c.Skills with
+        | Some sk -> yield printDC c sk
+        | None -> ()
+    ]
+
+    let buildBasics c = {
+        Title = None
+        Items = [
+            yield one ("Name", c.Name)
+            yield printTitle c
+            match c.Background with | Some bg -> yield one ("Background", bg) | None -> ()
+            yield one ("Hit Points", sprintf "%d" (Derive.hitPoints c))
+            match c.Size with | Some sz -> yield one ("Size", sz) | None -> ()
+            yield one ("Speed", Derive.speed c)
+        ]
+    }
+
+    let buildAbilities c = {
+        Title = Some "Abilities"
+        Items = [
+            for ab in Builder.AbilityOrder do
+                let score = Map.find ab c.Abilities
+                yield one (ab.ToString (), sprintf "%4d (%+2d)" score (Derive.modifier score))
+        ]
+    }
+
+    let buildArmor c = {
+        Title = Some "Armor"
+        Items = printArmorClass c
+    }
+
+    let buildMeleeWeapon c = {
+        Title = Some "Melee weapon"
+        Items = printWeaponStats (c.MeleeWeapon, c)
+    }
+
+    let buildRangedWeapon c = {
+        Title = Some "Ranged weapon"
+        Items = printWeaponStats (c.RangedWeapon, c)
+    }
+
+    let buildDifficultyClasses c = {
+        Title = Some "Difficulty classes"
+        Items = printClassDC c
+    }
+
+    let buildSaves c = {
+        Title = Some "Saves"
+        Items = Skills.saves |> List.map (printSkill c)
+    }
+
+    let buildSkills c = {
+        Title = Some "Skills"
+        Items = [
+            yield printSkill c Skills.perception
+            yield! Skills.regularSkillsForCharacter c |> List.map (printSkill c)
+        ]
+    }
+
+    let buildFeats c = {
+        Title = Some "Feats"
+        Items = c.Feats |> List.sortBy (fun f -> f.Name) |> List.map (fun f ->
+                one (f.Name, sprintf "page %3d" f.Page))
+    }
+
+    let create (c: Character) = [
+        yield buildBasics c
+        yield buildAbilities c
+        yield buildArmor c
+        yield buildMeleeWeapon c
+        yield buildRangedWeapon c
+        yield buildDifficultyClasses c
+        yield buildSaves c
+        yield buildSkills c
+        yield buildFeats c
+    ]
--- npcconsole/Program.fs
+++ npcconsole/Program.fs
@@ -7,60 +7,6 @@ open NpcConsole.Attributes
 
 // A console interaction.
 type ConsoleInteract () = 
-    let formatLevel c = Some (sprintf "Level %d" c.Level)
-    let formatClass c = match c.Class with | Some cl -> Some (sprintf "%A" cl) | None -> None
-    let print (name, value) = printfn "%30s: %s" name value
-
-    let printTitle c =
-        let elements =
-            [formatLevel c; c.Heritage; c.Ancestry; formatClass c]
-            |> List.choose id
-        print ("Is a", String.Join (" ", elements))
-
-    let printSkill c sk =
-        let rank = (Derive.rank sk c).ToString ()
-        let bonus = Derive.bonus sk c
-        print (sk.Name, sprintf "%+3d %15s (%15s)" bonus rank (sk.KeyAbility.ToString ()))
-
-    let printDC c sk =
-        let rank = (Derive.rank sk c).ToString ()
-        let dc = Derive.bonus sk c |> Derive.dc
-        print ((sprintf "%s DC" sk.Name), sprintf "%3d %15s (%15s)" dc rank (sk.KeyAbility.ToString ()))
-
-    let printArmorClass c =
-        match c.Armor with
-        | Some a ->
-            let rank = (Derive.rank a.Skill c).ToString ()
-            let ac = Derive.armorClass c
-            print ("Armor Class", sprintf "%3d %15s" ac rank)
-            print ("Armor Type", a.Name)
-            print ("Armor Traits", String.Join (", ", a.Traits |> List.map (fun t -> sprintf "%A" t)))
-        | None -> ()
-    
-    let printWeaponStats (weapon, c) =
-        match weapon with
-        | Some w ->
-            // TODO Dexterity bonus on melee weapon attack, sometimes
-            let sk = Skills.weaponSkill w
-            printSkill c sk
-            // TODO When do ranged weapons gain a damage bonus?  From what?  I forget...
-            let damageModifier =
-                match w.Type with
-                | Melee -> Map.find Strength c.Abilities |> Derive.modifier
-                | Ranged -> 0<Modifier>
-            print (sprintf "%s damage" w.Name, sprintf "%A %+2d (%A)" w.Damage damageModifier w.DamageType)
-            match w.Range with
-            | Some r -> print (sprintf "%s Range" w.Name, sprintf "%3d" r)
-            | None -> ()
-            if w.Reload > 0<Actions> then print (sprintf "%s Reload" w.Name, sprintf "%3d" w.Reload) else ()
-            print (sprintf "%s traits" w.Name, String.Join (", ", w.Traits |> List.map (fun t -> sprintf "%A" t)))
-        | None -> ()
-
-    let printClassDC c =
-        match Map.tryPick (fun (sk: Skill) _ -> if sk.Name.Contains ("Class") then Some sk else None) c.Skills with
-        | Some sk -> printDC c sk
-        | None -> ()
-
     interface IInteraction with
         member this.Prompt (prompt, choices) =
             printfn "Choose %s:" prompt
@@ -76,41 +22,18 @@ type ConsoleInteract () =
             |> Seq.pick (fun n -> List.tryPick (fun (n2, ch) -> if n2 = n then Some ch else None) numberedChoices)
 
         member this.Show c =
-            printfn ""
-            print ("Name", c.Name)
-            printTitle c
-            if Option.isSome c.Background then print ("Background", c.Background.Value)
-            print ("Hit Points", sprintf "%d" (Derive.hitPoints c))
-            if Option.isSome c.Size then print ("Size", c.Size.Value.ToString ())
-            print ("Speed", (Derive.speed c).ToString ())
-            printfn "Abilities:"
-            Builder.AbilityOrder |> List.iter (fun ab ->
-                let score = Map.find ab c.Abilities
-                print (ab.ToString (), sprintf "%4d (%+2d)" score (Derive.modifier score))
-            )
-            printfn "Armor:"
-            printArmorClass c
-            printfn "Melee weapon:"
-            printWeaponStats (c.MeleeWeapon, c)
-            printfn "Ranged weapon:"
-            printWeaponStats (c.RangedWeapon, c)
-            printfn "Difficulty Classes:"
-            printClassDC c
-            printfn "Saves:"
-            Skills.saves |> List.iter (printSkill c)
-            // TODO Armor, saving throws, weapon skills and that kind of thing
-            // Regular skills, in alphabetical order
-            printfn "Skills:"
-            printSkill c Skills.perception
-            Skills.regularSkillsForCharacter c |> List.iter (printSkill c)
-            // All feats, in alphabetical order
-            // TODO separate class features and that kind of thing?  Show in order
-            // of what level they were taken at, instead?  Show page numbers (that
-            // would be really useful for checking what they all mean!)
-            printfn "Feats:"
-            c.Feats |> List.sortBy (fun f -> f.Name) |> List.iter (fun f ->
-                print (f.Name, sprintf "page %3d" f.Page)
-            )
+            let sheet = CharacterSheet.create c
+
+            // I'll right justify the names and left justify the values:
+            let maxNameLength =
+                sheet
+                |> List.collect (fun s -> s.Items)
+                |> List.fold (fun acc (n, _) -> max acc n.Length) 0
+
+            for s in sheet do
+                match s.Title with | Some t -> printfn "%s" t | None -> ()
+                for (n, v) in s.Items do
+                    printfn "%s : %s" (n.PadLeft maxNameLength) v
 
 // Arguments
 type Args = {
