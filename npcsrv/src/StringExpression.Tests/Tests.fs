0f0d951223217a5d4d5381c2f8d5c92f2535bcd3
Alex Holloway <alex.holloway.ctx@gmail.com>
2020-03-15 20:19:18 +0000

    String expression utility

---
+++ npcsrv/src/StringExpression.Tests/Tests.fs
@@ -0,0 +1,112 @@
+module StringExpression.Tests
+
+open StringExpression
+open Xunit
+
+type Tests() =
+    [<Theory; InlineData(""); InlineData("test")>]
+    member this.``A single string can be returned`` (str: string) =
+        let expr = string {
+            return str
+        }
+        Assert.Equal(str, sprintf "%A" expr)
+
+    [<Theory; InlineData(true); InlineData(false)>]
+    member this.``A single boolean can be returned`` (b: bool) =
+        let expr = string {
+            return b
+        }
+        let expected = (sprintf "%b" b).ToUpperInvariant()
+        let actual = (sprintf "%A" expr).ToUpperInvariant()
+        Assert.Equal(expected, actual)
+
+    static member TestPairs : obj [] seq = seq {
+        yield [| "ook"; "eek" |]
+        yield [| "test"; "" |]
+        yield [| ""; "test" |]
+        yield [| "banana"; "fish" |]
+    }
+
+    [<Theory; MemberData("TestPairs")>]
+    member this.``Two strings are concatenated`` (str1: string) (str2: string) =
+        let expr = string {
+            yield str1
+            yield str2
+        }
+        Assert.Equal(str1 + str2, sprintf "%A" expr)
+
+    static member TestNumberPairs : obj [] seq = seq {
+        yield [| 1; 2 |]
+        yield [| 0; 14 |]
+        yield [| 2; 14 |]
+    }
+
+    [<Theory; MemberData("TestNumberPairs")>]
+    member this.``Two numbers are concatenated`` (n1: int) (n2: int) =
+        let expr = string {
+            yield n1
+            yield n2
+        }
+        Assert.Equal(sprintf "%d%d" n1 n2, sprintf "%A" expr)
+
+    [<Theory; MemberData("TestNumberPairs")>]
+    member this.``String expression can be deconstructed to numbers`` (n1: int) (n2: int) =
+        let expr = string {
+            let! n1s = string { n1 }            
+            let! n2s = string { n2 }
+            return n1s + n2s
+        }
+        Assert.Equal(sprintf "%d" (n1 + n2), sprintf "%A" expr)
+
+    static member TestLists : obj [] seq =
+        seq {
+            []
+            ["test"]
+            ["foo"; "bar"]
+            ["one"; "two"; "three"]
+            ["one"; ""; "two"; "three"]
+        } |> Seq.map (fun l -> [| l |])
+
+    [<Theory; MemberData("TestLists")>]
+    member this.``A list can be iterated over`` (strs: string list) =
+        let expr = string {
+            for s in strs do
+                yield s
+        }
+        let expected = strs |> List.fold (+) ""
+        Assert.Equal(expected, sprintf "%A" expr)
+
+    [<Theory; MemberData("TestLists")>]
+    member this.``A sequence can be iterated over`` (strs: string list) =
+        let strSeq = Seq.ofList strs
+        let expr = string {
+            for s in strSeq do
+                yield s
+        }
+        let expected = strs |> List.fold (+) ""
+        Assert.Equal(expected, sprintf "%A" expr)
+
+    [<Theory; MemberData("TestLists")>]
+    member this.``A string expression can be unpacked and iterated over`` (strs: string list) =
+        let expr1 = string {
+            for s in strs do
+                yield s
+        }
+        let expr2 = string {
+            for s in expr1 do
+                yield s + ".1"
+        }
+        let expected = strs |> List.map (fun s -> s + ".1") |> List.fold (+) ""
+        Assert.Equal(expected, sprintf "%A" expr2)
+
+    [<Theory; MemberData("TestLists")>]
+    member this.``A nested expression can be iterated over`` (strs: string list) =
+        let expr = string {
+            let! inner = string {
+                for s in strs do
+                    yield s
+            }
+            yield inner + ".1"
+        }
+        let expected = strs |> List.map (fun s -> s + ".1") |> List.fold (+) ""
+        Assert.Equal(expected, sprintf "%A" expr)
---
+++ npcsrv/src/StringExpression/Library.fs
@@ -0,0 +1,77 @@
+﻿module public StringExpression
+
+open System.Text
+
+// A computation expression for creating strings, a little like a StringBuilder.
+// TODO Make this into its own repository that builds a NuGet package :)
+[<StructuredFormatDisplay("{AsString}")>]
+type Stringified<'a> = { Items: 'a list; MakeString: StringBuilder -> StringBuilder }
+with
+    member this.AsString = this.ToString()
+    override this.ToString() =
+        let sb = StringBuilder() |> this.MakeString
+        sb.ToString()
+
+type StringExpression() =
+    let getItems s = s.Items
+
+    member this.Bind (m: Stringified<'a>, f) = this.For (m.Items, f)
+
+    member this.Combine (a: Stringified<'a>, b: Stringified<'a>) = {
+        Items = List.append a.Items b.Items
+        MakeString = a.MakeString >> b.MakeString
+    }
+
+    member this.Delay f = f()
+
+    member this.For (m: 'a list, f) =
+        this.Zero() |> List.foldBack (fun m2 s ->
+            let s2 = f m2
+            this.Combine (s2, s)) m
+
+    member this.For (m: 'a seq, f) =
+        this.Zero() |> Seq.foldBack (fun m2 s ->
+            let s2 = f m2
+            this.Combine (s2, s)) m
+
+    member this.For (m: Stringified<'a>, f) = this.Bind (m, f)
+
+    member this.Return (x: bool) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Return (x: char) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Return (x: char[]) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Return (x: decimal) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Return (x: double) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Return (x: int8) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Return (x: int16) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Return (x: int32) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Return (x: int64) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Return (x: string) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Return (x: uint8) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Return (x: uint16) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Return (x: uint32) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Return (x: uint64) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Return x = { Items = [x]; MakeString = fun sb -> x :> obj |> sb.Append }
+
+    member this.ReturnFrom m = m
+
+    member this.Yield (x: bool) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Yield (x: char) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Yield (x: char[]) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Yield (x: decimal) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Yield (x: double) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Yield (x: int8) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Yield (x: int16) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Yield (x: int32) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Yield (x: int64) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Yield (x: string) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Yield (x: uint8) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Yield (x: uint16) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Yield (x: uint32) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Yield (x: uint64) = { Items = [x]; MakeString = fun sb -> sb.Append x }
+    member this.Yield x = { Items = [x]; MakeString = fun sb -> x :> obj |> sb.Append }
+
+    member this.YieldFrom m = m
+
+    member this.Zero() = { Items = []; MakeString = id }
+
+let string = StringExpression()
